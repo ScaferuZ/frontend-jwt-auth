bba5f284a8d8cc26f7777d8e029d880f216f04eb
ScaferuZ <endrafruz@gmail.com>
2024-04-07 01:57:59 +0700

    feat: implement another route if user is authorized

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { checkIsUserLoggedIn } from "./redux/slices/authSlice";
+
 import Login from "./pages/Login";
 import AuthHeaderLayout from "./layout/authHeaderLayout";
 import Signup from "./pages/Signup";
+import MainHeaderLayout from "./layout/MainHeaderLayout";
+import TravelBlogPage from "./pages/TravelBlog";
 
 function App() {
   // authRouter will contain our entire routing data
@@ -25,7 +30,29 @@ function App() {
       </Route>,
     ),
   );
-  return <RouterProvider router={authRouter} />;
+
+  const mainRouter = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<MainHeaderLayout />}>
+          <Route path="/" element={<TravelBlogPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Route>,
+    ),
+  );
+  // initialitze dispatcher hook
+  const dispatcher = useDispatch();
+  // call the action function to check and update the isLoggedIn state
+  dispatcher(checkIsUserLoggedIn());
+  // fetch the isLoggedIn state variable using selector hook
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  // this component will run whenever there is a change in the isLoggedIn state
+  if (!isLoggedIn) {
+    return <RouterProvider router={authRouter} />;
+  }
+  return <RouterProvider router={mainRouter} />;
 }
 
 export default App;
